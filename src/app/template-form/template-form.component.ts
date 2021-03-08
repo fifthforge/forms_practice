f42e1f899dec7abe5918139aca0e9be6532d7eb8
fifthforge <anton_at_work@hotmail.com>
2021-03-08 15:14:01 +0200

    add functionality to the form

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -13,4 +14,22 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /* <select> functions */
+  topics: string[] = ["All", "Topic A", "Topic B", "Topic C"]
+  /* Adds a style class to <select> that changes font color from placeholder to default */
+  optionSelected = false;
+  topicSelected() {
+    this.optionSelected = true;
+  }
+
+  formSubmitAttempted = false;
+  subscribe(loginForm: NgForm, submit) {
+    if (loginForm.value.selectTopic == "") loginForm.value.selectTopic = "All"
+    this.formSubmitAttempted = true;
+    if (loginForm.status == "INVALID") {
+      console.log("Form invalid")
+    } else {
+      console.log(loginForm.value, loginForm.valid, submit)
+    }
+  }
 }
